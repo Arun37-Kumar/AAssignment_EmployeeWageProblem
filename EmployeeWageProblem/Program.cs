da643e34c853f0deb77353c3f95ef16a99718ff3
Arun37-Kumar <arun007624@gmail.com>
2022-03-31 07:44:11 +0530

    UC2 updated

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -12,18 +12,31 @@ namespace EmployeeWageProblem
         {
             Console.WriteLine("Welcome to Employee Wage Problem : ");
 
-            //UC1
+            //Constants
             int PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
+
             int isCheck = random.Next(0, 2);
             if (isCheck == PRESENT)
             {
                 Console.WriteLine("Employee is present.");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent.");
+                empHrs = 0;
             }
+            
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage : {0} ", empWage);
+            Console.ReadLine();
+
         }
 
     }
